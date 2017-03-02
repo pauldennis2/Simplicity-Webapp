dd6bbce6603a757c11edb1fb0d77c5776fa457dd
Paul Dennis <pauldennis.misc@gmail.com>
2017-03-02 16:31:52 -0500

    Added readme and some help content

---
+++ src/main/java/com/tiy/webapp/starsys/ComplexPlanet.java
@@ -0,0 +1,19 @@
+package com.tiy.webapp.starsys;
+
+/**
+ * Created by Paul Dennis on 3/2/2017.
+ */
+public class ComplexPlanet {
+
+    String name;
+    private int size; //Ranges from 6 to 20. Home planet is 12, average is 10.
+    private int population;
+    private double growthRate; // 0.01 to 0.05. Home planet is 0.04, average is 0.03.
+    private double productionEfficiency;// 0.5 to 2.0. Home planet 1.0, average 1.0
+
+    //weapons/shields/generators/damage
+    /*
+    In general planets should have a lot of energy reserves and shield health, but poor max damage absorption. (They do
+    OK against a few fighters but will get overwhelmed quickly by a fleet of larger ships.
+     */
+}
